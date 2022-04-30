483e0d223016f6e0907a24a2baafa6b98686ddf6
CarlosMonzalvoN <carlos.monzalvo.n@gmail.com>
2022-04-29 22:19:19 -0500

    Initial Commit

---
+++ Cryptolator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Cryptolator
+//
+//  Created by Pedro Carlos Monzalvo Navarro on 29/04/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
